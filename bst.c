45189ab6e5d338e9804f8b992df7daed7c8b656c
smmitchell2 <smmitchell3@gmail.com>
2017-03-03 15:25:47 -0600

    not much

---
+++ bst.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "bst.h"
+
+bst *newBST(void (*)(FILE *,void *),int (*)(void *,void *)){
+
+}
+
+bstNode *insertBST(bst *tree, void *data){
+
+}
+
+int findBST(bst *tree, void *data){
+
+}
+
+bstNode *findBSTNode(bst *tree, void *data){
+
+}
+
+bstNode *swapToLeafBSTNode(bstNode *n){
+
+}
+
+void pruneBSTNode(bst *tree,bstNode *n){
+
+}
+
+int sizeBST(bst *tree){
+
+}
+
+void statisticsBST(bst *tree,FILE *FP){
+
+}
+
+void displayBST(FILE *FP,bst *tree){
+
+}
+
+void checkBST(bst *tree){
+
+}
