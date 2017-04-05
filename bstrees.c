fe47ff1ada68444f30ce7386e5fe3429fd94473d
Shawn Mitchell <smmitchell3@gmail.com>
2017-04-05 18:22:30 -0500

    stuff

--- bstrees.c
+++ bstrees.c
@@ -1,11 +1,227 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "queue.h"
-#include "vbst.h"
+#include <ctype.h>
+#include <string.h>
 #include "bst.h"
+#include "vbst.h"
 #include "rbt.h"
+#include "scanner.h"
+#include "comparator.h"
 
-int main(int argc,char **argv){
+void displayString(FILE *fp, void *v){
+    fprintf(fp, "\"%s\"", (char*)v);
+}
 
-    return 0;
+void cleanUp(char *word){
+    char *src = word, *dst = word;
+	while (*src){
+	    if(ispunct((unsigned char)*src)){
+		    src++;
+		}
+		else if(isupper((unsigned char)*src)){
+			*dst++ = tolower((unsigned char)*src);
+			src++;
+		}
+		else if(isdigit((unsigned char)*src)){
+			src++;
+		}
+		else if(isspace(*src) && isspace(*(src+1))){
+			while(isspace(*(src+1))){
+				src++;
+			}
+		}
+		else if(isspace(*src)){
+			*src = ' ';
+			*dst++ = *src++;
+		}
+		else if(!islower((unsigned char)*src)){
+			src++;
+		}
+		else if(src == dst){
+			src++;
+			dst++;
+		}
+		else{
+			*dst++ = *src++;
+		}
+	}
+	*dst = 0;
+}
+
+int main(int argc, char **argv){
+	FILE *data = fopen(argv[2], "r");
+	FILE *commands = fopen(argv[3], "r");
+	FILE *output = stdout;
+	if(argc == 4){
+		output = stdout;
+	}
+	else if(argc == 5){
+		output = fopen(argv[4], "w");
+	}
+	else{
+		printf("Invalid number of arguments passed\n");;
+		return 0;
+	}
+	if(strcmp(argv[1], "-r") == 0){
+		char variable = readChar(data);
+		char *var;
+		rbt *r = newRBT(displayString, stringComparator);
+		while(!feof(data)){
+			if(variable == '"'){
+				ungetc(variable, data);
+				var = readString(data);
+			}
+			else{
+				ungetc(variable, data);
+				var = readToken(data);
+			}
+			cleanUp(var);
+			if(strcmp(var, "") != 0){
+				insertRBT(r, var);
+			}
+			variable = readChar(data);
+		}
+		char commandVariable = readChar(commands);
+		while(!feof(commands)){
+			if(commandVariable == 'i'){
+				char *tempCommand;
+				commandVariable = readChar(commands);
+				if(commandVariable == '"'){
+					ungetc(commandVariable, commands);
+					tempCommand = readString(commands);
+				}
+				else{
+					ungetc(commandVariable, commands);
+					tempCommand = readToken(commands);
+				}
+				cleanUp(tempCommand);
+				if(strcmp(tempCommand, "") != 0){
+					insertRBT(r, tempCommand);
+				}
+			}
+			else if(commandVariable == 'd'){
+				char *tempCommand;
+				commandVariable = readChar(commands);
+				if(commandVariable == '"'){
+					ungetc(commandVariable, commands);
+					tempCommand = readString(commands);
+				}
+				else{
+					ungetc(commandVariable, commands);
+					tempCommand = readToken(commands);
+				}
+				cleanUp(tempCommand);
+			}
+			else if(commandVariable == 'f'){
+				char *tempCommand;
+				commandVariable = readChar(commands);
+				if(commandVariable == '"'){
+					ungetc(commandVariable, commands);
+					tempCommand = readString(commands);
+				}
+				else{
+					ungetc(commandVariable, commands);
+					tempCommand = readToken(commands);
+				}
+				cleanUp(tempCommand);
+				fprintf(output, "Frequency of \"%s\": %d\n", tempCommand, findRBT(r, tempCommand));
+			}
+			else if(commandVariable == 's'){
+				displayRBT(output, r);
+			}
+			else if(commandVariable == 'r'){
+				statisticsRBT(r, output);
+			}
+			else{
+				printf("Invalid command detected\n");
+				return 0;
+			}
+			commandVariable = readChar(commands);
+		}
+	}
+	else if(strcmp(argv[1], "-v") == 0){
+		char variable = readChar(data);
+		char *var;
+		vbst *v = newVBST(displayString, stringComparator);
+		while(!feof(data)){
+			if(variable == '"'){
+				ungetc(variable, data);
+				var = readString(data);
+			}
+			else{
+				ungetc(variable, data);
+				var = readToken(data);
+			}
+			cleanUp(var);
+			if(strcmp(var, "") != 0){
+				insertVBST(v, var);
+			}
+			variable = readChar(data);
+		}
+		char commandVariable = readChar(commands);
+		while(!feof(commands)){
+			if(commandVariable == 'i'){
+				char *tempCommand;
+				commandVariable = readChar(commands);
+				if(commandVariable == '"'){
+					ungetc(commandVariable, commands);
+					tempCommand = readString(commands);
+				}
+				else{
+					ungetc(commandVariable, commands);
+					tempCommand = readToken(commands);
+				}
+				cleanUp(tempCommand);
+				if(strcmp(tempCommand, "") != 0){
+					insertVBST(v, tempCommand);
+				}
+			}
+			else if(commandVariable == 'd'){
+				char *tempCommand;
+				commandVariable = readChar(commands);
+				if(commandVariable == '"'){
+					ungetc(commandVariable, commands);
+					tempCommand = readString(commands);
+				}
+				else{
+					ungetc(commandVariable, commands);
+					tempCommand = readToken(commands);
+				}
+				cleanUp(tempCommand);
+				if(strcmp(tempCommand, "") != 0){
+					deleteVBST(v, tempCommand);
+				}
+			}
+			else if(commandVariable == 'f'){
+				char *tempCommand;
+				commandVariable = readChar(commands);
+				if(commandVariable == '"'){
+					ungetc(commandVariable, commands);
+					tempCommand = readString(commands);
+				}
+				else{
+					ungetc(commandVariable, commands);
+					tempCommand = readToken(commands);
+				}
+				cleanUp(tempCommand);
+				fprintf(output, "Frequency of \"%s\": %d", tempCommand, findVBST(v, tempCommand));
+			}
+			else if(commandVariable == 's'){
+				displayVBST(output, v);
+			}
+			else if(commandVariable == 'r'){
+				statisticsVBST(v, output);
+			}
+			else{
+				printf("Invalid command detected\n");
+				return 1;
+			}
+			commandVariable = readChar(commands);
+		}
+	}
+	else{
+		printf("Invalid argument.\n");
+		return 0;
+	}
+	return 0;
 }
--- rbt.c
+++ rbt.c
@@ -1,6 +1,311 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "queue.h"
 #include "bst.h"
 #include "rbt.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct rbtValue
+{
+	void *value;
+	int freq;
+	int color;
+	void (*display)(FILE *, void *);
+	int (*compare)(void *, void *);
+} rbtValue;
+
+static rbtValue *newRBTValue(rbt *, void *);
+static void rbtDisplay(FILE *, void *);
+
+static int rbtComparator(void *x, void *y)
+{
+	if(x == NULL || y == NULL)
+	{
+		return 1;
+	}
+	rbtValue *a = x;
+	rbtValue *b = y;
+	if(a->value == NULL || b->value == NULL)
+	{
+		return 1;
+	}
+	return a->compare(a->value, b->value);
+}
+
+static int nodeComparator(void *x, void *y)
+{
+	if(x == NULL || y == NULL)
+	{
+		return 1;
+	}
+	bstNode *temp1 = x;
+	bstNode *temp2 = y;
+	rbtValue *a = temp1->value;
+	rbtValue *b = temp2->value;
+	if(a->value == NULL || b->value == NULL)
+	{
+		return 1;
+	}
+	return a->compare(a->value, b->value);
+}
+
+rbt *newRBT(void (*display)(FILE *, void *), int(*comparator)(void *, void *))
+{
+	rbt *newRBT = malloc(sizeof(rbt));
+	newRBT->tree = newBST(rbtDisplay, rbtComparator);
+	newRBT->display = display;
+	newRBT->compare = comparator;
+	newRBT->size = 0;
+	newRBT->words = 0;
+	return newRBT;
+}
+
+static rbtValue *newRBTValue(rbt *r, void *value)
+{
+	rbtValue *newRBTValue = malloc(sizeof(rbtValue));
+	newRBTValue->value = value;
+	newRBTValue->freq = 1;
+	newRBTValue->display = r->display;
+	newRBTValue->compare = r->compare;
+	newRBTValue->color = 0;
+	return newRBTValue;
+}
+
+static void replaceNode(rbt *r, bstNode *oldNode, bstNode *newNode)
+{
+
+	if(nodeComparator(oldNode, r->tree->root) == 0)
+	{
+		r->tree->root = newNode;
+	}
+	else
+	{
+		if(nodeComparator(oldNode, oldNode->parent->left) == 0)
+		{
+			oldNode->parent->left = newNode;
+		}
+		else
+		{
+			oldNode->parent->right = newNode;
+		}
+	}
+	if(newNode != NULL)
+	{
+		newNode->parent = oldNode->parent;
+	}
+}
+
+static void rightRotate(rbt *r, bstNode *node)
+{
+	bstNode *leftNode = node->left;
+	replaceNode(r, node, leftNode);
+	node->left = leftNode->right;
+	if(leftNode->right != NULL)
+	{
+		leftNode->right->parent = node;
+	}
+	leftNode->right = node;
+	node->parent = leftNode;
+}
+
+static void leftRotate(rbt *r, bstNode *node)
+{
+	bstNode *rightNode = node->right;
+	replaceNode(r, node, rightNode);
+	node->right = rightNode->left;
+	if(rightNode->left != NULL)
+	{
+		rightNode->left->parent = node;
+	}
+	rightNode->left = node;
+	node->parent = rightNode;
+}
+
+static int color(rbtValue *rv)
+{
+	if(rv == NULL)
+	{
+		return 0;
+	}
+	else
+	{
+		return rv->color;
+	}
+}
+
+
+static bstNode *uncle(rbt *r, bstNode *b)
+{
+	if(b->parent == NULL || b->parent->parent->left == NULL)
+	{
+		return r->tree->root;
+	}
+	if(nodeComparator(b->parent, b->parent->parent->left) == 0)
+	{
+		return b->parent->parent->right;
+	}
+	else
+	{
+		return b->parent->parent->left;
+	}
+	return r->tree->root;
+}
+
+
+static void insertionFixUp(rbt *r, bstNode *node)
+{
+	while(1)
+	{
+		if(nodeComparator(node, node->parent) == 0)
+		{
+			break;
+		}
+		if(color(node->parent->value) == 0)
+		{
+			break;
+		}
+		bstNode *unc = uncle(r, node);
+		int color1 = 0;
+		if(unc == NULL)
+		{
+			rbtValue *tempValue = newRBTValue(r, "the");
+			tempValue->color = 0;
+			color1 = 0;
+		}
+		else
+		{
+			color1 = color(unc->value);
+		}
+		if(color1 == 1)
+		{
+			rbtValue *parentValue = node->parent->value;
+			parentValue->color = 0;
+			rbtValue *uncleValue = uncle(r, node)->value;
+			uncleValue->color = 0;
+			rbtValue *grandparentValue = node->parent->parent->value;
+			grandparentValue->color = 1;
+			node = node->parent->parent;
+		}
+		else
+		{
+		
+			if((nodeComparator(node, node->parent->right) == 0) && (nodeComparator(node->parent, node->parent->parent->left) == 0))
+			{
+				bstNode *oldParent = node->parent;
+				bstNode *oldNode = node;
+				leftRotate(r, node->parent);
+				node = oldParent;
+				node->parent = oldNode;
+			
+			}
+			if((nodeComparator(node, node->parent->left) == 0) && (nodeComparator(node->parent, node->parent->parent->right) == 0))
+			{
+				bstNode *oldParent = node->parent;
+				bstNode *oldNode = node;
+				rightRotate(r, node->parent);
+				node = oldParent;
+				node->parent = oldNode;
+			}
+			rbtValue *parentValue = node->parent->value;
+			parentValue->color = 0;
+			rbtValue *grandparentValue = node->parent->parent->value;
+			grandparentValue->color = 1;
+			if(nodeComparator(node->parent, node->parent->parent->right) == 0)
+			{
+				leftRotate(r, node->parent->parent);
+			}
+			else if(nodeComparator(node->parent, node->parent->parent->left) == 0)
+			{
+				rightRotate(r, node->parent->parent);
+			}
+			break;
+		}
+		
+	}
+	rbtValue *root = r->tree->root->value;
+	r->tree->root->parent = r->tree->root;
+	root->color = 0;
+}
+
+
+
+
+void insertRBT(rbt *r, void *value)
+{
+	rbtValue *newValue = newRBTValue(r, value);
+	int theValue = findBST(r->tree, newValue);
+	if(theValue == 0)
+	{
+		(void)insertBST(r->tree, newValue);
+		bstNode *temp = findBSTNode(r->tree, newValue);
+		rbtValue *tempValue = temp->value;
+		tempValue->color = 1;
+		insertionFixUp(r, findBSTNode(r->tree, tempValue));
+		r->size++;
+		r->words++;
+	}
+	else
+	{
+		bstNode *temp = findBSTNode(r->tree, newValue);
+		rbtValue *temporary = temp->value;
+		temporary->freq += 1;
+		r->size++;
+	}
+}
+
+int findRBT(rbt *r, void *value)
+{
+	rbtValue *newValue = newRBTValue(r, value);
+	bstNode *node = findBSTNode(r->tree, newValue);
+	if(node == NULL)
+	{
+		return 0;
+	}
+	rbtValue *val = node->value;
+	return val->freq;
+}
+
+int sizeRBT(rbt *r)
+{
+	return r->size;
+}
+
+int wordsRBT(rbt *r)
+{
+	return r->words;
+}
+
+void deleteRBT(rbt *r, void *v)
+{
+	(void)r;
+	(void)v;
+}
+
+void statisticsRBT(rbt *r, FILE *fp)
+{
+	fprintf(fp, "Words/Phrases: %d\n", sizeRBT(r));
+	statisticsBST(r->tree, fp);
+}
+
+void rbtDisplay(FILE *fp, void *v)
+{
+	rbtValue *value = v;
+	value->display(fp, value->value);
+	int freq = value->freq;
+	if(freq > 1)
+	{
+		fprintf(fp, "-%d", freq);
+	}
+	if(value->color == 0)
+	{
+		fprintf(fp, "-B");
+	}
+	if(value->color == 1)
+	{
+		fprintf(fp, "-R");
+	}
+}
+
+void displayRBT(FILE *fp, rbt *r)
+{
+	displayBST(fp, r->tree);
+}
 
